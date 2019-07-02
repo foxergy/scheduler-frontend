34c87acb7b050a9b788cb76c78553251ecfeb09e
Der Anonyme <foxergy@gmail.com>
2019-07-02 15:50:39 +0200

    Refactoring

--- src/app/components/created-event-modal/created-event-modal.component.spec.ts
+++ src/app/components/created-event-modal/created-event-modal.component.spec.ts
@@ -1,7 +1,7 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CreatedEventModalComponent } from './created-event-modal.component';
+import { CreatedEventModalComponent } from "./created-event-modal.component";
 
 describe('CreatedEventModalComponent', () => {
   let component: CreatedEventModalComponent;
